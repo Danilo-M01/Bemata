75c72e73c6475005419695bb9a83d10cd64c44ae
Danilo-M01 <danilomomirovic95@gmail.com>
2026-05-15 19:00:09 +0200

    Add rewrite to next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/index.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
